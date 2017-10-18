37c733c00ec9c8e2d1f26a69ebef7b937a4b3a3b
Andrew <andreiblinets@gmail.com>
2017-10-18 14:26:15 +0300

    Create base package and class

---
+++ src/main/java/com/company/andrewblinets/Main.java
@@ -0,0 +1,7 @@
+package com.company.andrewblinets;
+
+/**
+ * Created by Андрей on 18.10.2017.
+ */
+public class Main {
+}
---
+++ src/main/java/com/company/andrewblinets/userinterfase/UserInterfase.java
@@ -0,0 +1,7 @@
+package com.company.andrewblinets.userinterfase;
+
+/**
+ * Created by Андрей on 18.10.2017.
+ */
+public class UserInterfase {
+}
